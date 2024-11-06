4738d43034af0d37eadb92c4a53f112d397dabaf
serhat <serhat@DESKTOP-MUEONC4>
2024-11-06 15:55:22 +0300

    loglama mail eksik

--- WebAPI/Configurations/ColumnWriters/UsernameEnricher.cs
+++ WebAPI/Configurations/ColumnWriters/UsernameEnricher.cs
@@ -1,17 +1,37 @@
 ﻿using Serilog.Core;
 using Serilog.Events;
+using Microsoft.AspNetCore.Http;
+using System.Security.Claims;
 
-public class UsernameEnricher : ILogEventEnricher
+namespace YourNamespace.Configurations
 {
-    private readonly string _username;
-
-    public UsernameEnricher(string username)
+    public class UsernameEnricher : ILogEventEnricher
     {
-        _username = username;
-    }
+        private readonly IHttpContextAccessor _httpContextAccessor;
 
-    public void Enrich(LogEvent logEvent, ILogEventPropertyFactory propertyFactory)
-    {
-        logEvent.AddPropertyIfAbsent(propertyFactory.CreateProperty("user_name", _username));
+        // Parametresiz yapıcı ekledik, ancak IHttpContextAccessor injection yapacağız
+        public UsernameEnricher() { }
+
+        // IHttpContextAccessor'ı dışarıdan alıyoruz
+        public UsernameEnricher(IHttpContextAccessor httpContextAccessor)
+        {
+            _httpContextAccessor = httpContextAccessor;
+        }
+
+        public void Enrich(LogEvent logEvent, ILogEventPropertyFactory propertyFactory)
+        {
+            var user = _httpContextAccessor?.HttpContext?.User;
+
+            if (user == null || !user.Identity.IsAuthenticated)
+            {
+                logEvent.AddPropertyIfAbsent(propertyFactory.CreateProperty("user_email", "Anonymous"));
+                return;
+            }
+
+            // Kullanıcının e-posta adresini alıyoruz
+            var email = user.FindFirst(ClaimTypes.Email)?.Value;
+
+            logEvent.AddPropertyIfAbsent(propertyFactory.CreateProperty("user_email", email ?? "Unknown"));
+        }
     }
 }
