af93681d4f8f469b3b56024cfae09825c1d68900
Serhat <115489292+serhatlyildiz@users.noreply.github.com>
2024-11-28 11:39:21 +0300

    28.11.2924

--- Core/CrossCuttingConcerns/Caching/Microsoft/MemoryCacheManager.cs
+++ Core/CrossCuttingConcerns/Caching/Microsoft/MemoryCacheManager.cs
@@ -2,6 +2,8 @@
 using Core.Utilities.IoC;
 using Microsoft.Extensions.Caching.Memory;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Identity.Client;
+using System.Diagnostics;
 using System.Text.RegularExpressions;
 
 public class MemoryCacheManager : ICacheManager
@@ -14,12 +16,32 @@ public class MemoryCacheManager : ICacheManager
         _memoryCache = ServiceTool.ServiceProvider.GetService<IMemoryCache>();
     }
 
+//    public class Sepet
+//    {
+//        public int SepetID { get; set; }
+//        public int KullaniciID { get; set; }
+//        public List<Malzeme> sepetiniçindemalzemer  { get; set; }
+//}
+
     public void Add(string key, object value, int duration)
     {
         _memoryCache.Set(key, value, TimeSpan.FromMinutes(duration));
 
         // Anahtar listesini güncelle
         var keys = _memoryCache.Get<List<string>>(CacheKeyList) ?? new List<string>();
+
+
+        //var s = new Sepet
+        //{
+        //    SepetID = 1,
+        //    KullaniciID = 1,
+        //    sepetiniçindemalzemer = ["kalem", "kitap"],
+        //};
+
+        
+        //_memoryCache.Set($"sepet{s.KullaniciID}", s, TimeSpan.FromMinutes(10));
+
+        //var test = _memoryCache.Get<Sepet>($"lstSepet{s.KullaniciID}");
         if (!keys.Contains(key))
         {
             keys.Add(key);
